aa79cf7e64a1048fdf2ac2736ac36d648db1f84e
shinyamaeda <shinyamaeda@shinyamaedas-MacBook-Pro.local>
2016-10-19 18:59:51 +0900

    m

---
+++ EpicRapBattle/EpicRapBattle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EpicRapBattle
+//
+//  Created by shinyamaeda on 10/19/16.
+//  Copyright © 2016 shinyamaeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("\(#function): start")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        print("\(#function): start")
+    }
+
+
+}
+
